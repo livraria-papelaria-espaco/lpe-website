26c652100d0836a14010fc0562d81d02d53a3b0e
Diogo Correia <me@diogotc.com>
2020-03-18 23:53:12 +0000

    Add searchbar to category page

--- frontend/pages/category/[category].js
+++ frontend/pages/category/[category].js
@@ -5,13 +5,15 @@ import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import Layout from '~/components/Layout';
 import CategoryList from '~/components/products/CategoryList';
-import ProductQuery from '~/components/products/ProductQuery';
 import ProductFilterPrice from '~/components/products/ProductFilterPrice';
+import ProductQuery from '~/components/products/ProductQuery';
+import SearchBar from '~/components/products/SearchBar';
 
 const priceRange = [0, 100];
 
 const CategoryPage = () => {
   const [priceFilter, setPriceFilter] = useState(priceRange);
+  const [search, setSearch] = useState('');
   const router = useRouter();
   const { category } = router.query;
   const { data } = useQuery(GET_CATEGORY_FROM_SLUG, { variables: { category } });
@@ -24,17 +26,16 @@ const CategoryPage = () => {
     <Layout title={categoryTitle}>
       <Grid container spacing={2}>
         <Grid item xs={12} md={3}>
+          <SearchBar setValue={setSearch} />
           <CategoryList />
           <ProductFilterPrice value={priceRange} setValue={setPriceFilter} />
         </Grid>
         <Grid item xs={12} md={9}>
           <ProductQuery
-            where={{
-              category: { slug: category },
-              price_gte: priceFilter[0],
-              price_lte: priceFilter[1],
-            }}
             sort='createdAt:desc'
+            priceRange={priceFilter}
+            search={search}
+            category={category}
           />
         </Grid>
       </Grid>
