dbbb48715acee1a071990f5173ed65ea8d8cae9f
Diogo Correia <me@diogotc.com>
2020-03-23 23:09:02 +0000

    Remove REST routes and add data validations to "category"

--- backend/api/category/controllers/category.js
+++ backend/api/category/controllers/category.js
@@ -5,6 +5,8 @@ const { sanitizeEntity } = require('strapi-utils');
 module.exports = {
   async findOne(ctx) {
     const slug = ctx.params.slug || ctx.params._slug;
+    if (!slug) return null;
+
     const entity = await strapi.services.category.findOne({ slug });
     if (!entity || entity.show === false) return null;
     return sanitizeEntity(entity, { model: strapi.models.category });
