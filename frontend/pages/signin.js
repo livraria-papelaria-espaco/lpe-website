0ba24f0403fd5e38e83018a6871c8de938d8a333
Diogo Correia <me@diogotc.com>
2020-03-26 20:25:34 +0000

    Redesign login/register pages

--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,27 +1,96 @@
-import { Button, Container, Paper, TextField, Typography } from '@material-ui/core';
+import {
+  Button,
+  Container,
+  Link as MUILink,
+  makeStyles,
+  Paper,
+  TextField,
+  Typography,
+  CircularProgress,
+} from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
+import BackIcon from '@material-ui/icons/ArrowBackRounded';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import Layout from '~/components/Layout';
 import { useAuth } from '~/hooks/useAuth';
+import LogoSvg from '../assets/logo.svg'; //TODO fix relative import
+
+const useStyles = makeStyles((theme) => ({
+  logo: {
+    fill: theme.palette.primary.main,
+    maxWidth: 200,
+    textAlign: 'center',
+    marginBottom: theme.spacing(2),
+  },
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+  },
+  paper: {
+    padding: theme.spacing(4),
+    width: '100%',
+    textAlign: 'center',
+  },
+  fieldDiv: {
+    margin: theme.spacing(2),
+    maxWidth: 500,
+  },
+  buttonArea: {
+    padding: theme.spacing(2),
+    width: '100%',
+    position: 'relative',
+  },
+  backContainer: {
+    textAlign: 'left',
+  },
+  backLink: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  submitButton: {
+    background: `linear-gradient(45deg, ${theme.palette.primary.dark} 20%, ${theme.palette.primary.light} 70%)`,
+    '&$buttonDisabled': {
+      background: `rgba(0, 0, 0, 0.12)`,
+    },
+  },
+  buttonDisabled: {},
+  alert: {
+    margin: theme.spacing(2),
+  },
+  loading: {
+    color: theme.palette.secondary.main,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -12,
+    marginLeft: -12,
+  },
+}));
 
 const SignIn = () => {
+  const classes = useStyles();
   const router = useRouter();
   const { isAuthenticated, login } = useAuth(); // TODO use "loading" and wait before displaying form
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState(false);
 
   const onEmailChange = (evt) => setEmail(evt.target.value);
   const onPasswordChange = (evt) => setPassword(evt.target.value);
 
-  const onSubmit = async () => {
+  const onSubmit = async (e) => {
+    e.preventDefault();
     setLoading(true);
     try {
       await login(email, password);
     } catch (e) {
-      setError('Email e/ou palavra passe incorretos');
+      setError(true);
       setLoading(false);
     }
   };
@@ -32,12 +101,25 @@ const SignIn = () => {
 
   return (
     <Layout title='Iniciar Sessão' hideNavbar>
-      <Container maxWidth='sm'>
-        <Paper>
-          <Typography variant='body1'>{error}</Typography>
-          {loading && <Typography variant='body1'>Loading...</Typography>}
-          <form>
-            <div>
+      <Container maxWidth='sm' className={classes.root}>
+        <Paper className={classes.paper}>
+          <div className={classes.backContainer}>
+            <MUILink href='#' onClick={router.back} className={classes.backLink} underline='none'>
+              <BackIcon />
+              <strong>Voltar</strong>
+            </MUILink>
+          </div>
+          <LogoSvg className={classes.logo} />
+          <Typography variant='h4' component='h1'>
+            Iniciar Sessão
+          </Typography>
+          {error && (
+            <Alert onClose={() => setError(false)} severity='error' className={classes.alert}>
+              Dados de início de sessão incorretos!
+            </Alert>
+          )}
+          <form noValidate>
+            <div className={classes.fieldDiv}>
               <TextField
                 id='email'
                 label='Email'
@@ -45,9 +127,10 @@ const SignIn = () => {
                 onChange={onEmailChange}
                 type='email'
                 variant='outlined'
+                fullWidth
               />
             </div>
-            <div>
+            <div className={classes.fieldDiv}>
               <TextField
                 id='password'
                 label='Palavra Passe'
@@ -55,12 +138,34 @@ const SignIn = () => {
                 onChange={onPasswordChange}
                 type='password'
                 variant='outlined'
+                fullWidth
               />
             </div>
-            <Button color='primary' onClick={onSubmit}>
-              Iniciar Sessão
-            </Button>
+            <div className={classes.buttonArea}>
+              <Button
+                type='submit'
+                color='primary'
+                onClick={onSubmit}
+                variant='contained'
+                fullWidth
+                classes={{
+                  root: classes.submitButton,
+                  disabled: classes.buttonDisabled,
+                }}
+                disabled={loading}
+              >
+                Iniciar Sessão
+              </Button>
+              {loading && <CircularProgress size={24} className={classes.loading} />}
+            </div>
           </form>
+          <Typography variant='body2'>
+            <Link href='/signup' passHref>
+              <MUILink>
+                <strong>Ainda não tem uma conta? Registe-se!</strong>
+              </MUILink>
+            </Link>
+          </Typography>
         </Paper>
       </Container>
     </Layout>
--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,29 +1,127 @@
-import { Button, Container, Paper, TextField, Typography } from '@material-ui/core';
+import {
+  Button,
+  Container,
+  Link as MUILink,
+  makeStyles,
+  Paper,
+  TextField,
+  Typography,
+  CircularProgress,
+} from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
+import BackIcon from '@material-ui/icons/ArrowBackRounded';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import Layout from '~/components/Layout';
 import { useAuth } from '~/hooks/useAuth';
+import LogoSvg from '../assets/logo.svg'; //TODO fix relative import
+
+const emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const usernameRegExp = /^[A-Za-z0-9_ ]{4,}$/;
+const passwordRegExp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$/;
+
+const useStyles = makeStyles((theme) => ({
+  logo: {
+    fill: theme.palette.primary.main,
+    maxWidth: 200,
+    textAlign: 'center',
+    marginBottom: theme.spacing(2),
+  },
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+  },
+  paper: {
+    padding: theme.spacing(4),
+    width: '100%',
+    textAlign: 'center',
+  },
+  fieldDiv: {
+    margin: theme.spacing(2),
+    maxWidth: 500,
+  },
+  buttonArea: {
+    padding: theme.spacing(2),
+    width: '100%',
+    position: 'relative',
+  },
+  backContainer: {
+    textAlign: 'left',
+  },
+  backLink: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  submitButton: {
+    background: `linear-gradient(45deg, ${theme.palette.primary.dark} 20%, ${theme.palette.primary.light} 70%)`,
+    '&$buttonDisabled': {
+      background: `rgba(0, 0, 0, 0.12)`,
+    },
+  },
+  buttonDisabled: {},
+  alert: {
+    margin: theme.spacing(2),
+  },
+  loading: {
+    color: theme.palette.secondary.main,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -12,
+    marginLeft: -12,
+  },
+}));
 
 const SignUp = () => {
+  const classes = useStyles();
   const router = useRouter();
   const { isAuthenticated, register } = useAuth();
 
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
 
   const onUsernameChange = (evt) => setUsername(evt.target.value);
   const onEmailChange = (evt) => setEmail(evt.target.value);
   const onPasswordChange = (evt) => setPassword(evt.target.value);
+  const onPasswordConfirmationChange = (evt) => setPasswordConfirmation(evt.target.value);
 
-  const onSubmit = async () => {
+  const usernameError = !usernameRegExp.test(username);
+  const emailError = !emailRegExp.test(email);
+  const passwordError = !passwordRegExp.test(password);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (usernameError) {
+      setError(
+        'O nome de utilizador tem de ter, no mínimo, 4 catacteres e só pode conter letras, números, _ e espaços'
+      );
+      return;
+    }
+    if (emailError) {
+      setError('Email inválido');
+      return;
+    }
+    if (passwordError) {
+      setError('A palavra passe deverá conter, pelo menos, 8 caracteres e um dígito');
+      return;
+    }
+    if (password !== passwordConfirmation) {
+      setError('As palavra passes não correspondem');
+      return;
+    }
     setLoading(true);
     try {
       await register(username, password, email);
     } catch (e) {
-      setError('Ocorreu um erro ao criar uma conta');
+      //TODO get errors from backend
+      setError('Email e/ou username já em uso');
       setLoading(false);
     }
   };
@@ -33,13 +131,26 @@ const SignUp = () => {
   }, [isAuthenticated]);
 
   return (
-    <Layout title='Iniciar Sessão' hideNavbar>
-      <Container maxWidth='sm'>
-        <Paper>
-          <Typography variant='body1'>{error}</Typography>
-          {loading && <Typography variant='body1'>Loading...</Typography>}
-          <form>
-            <div>
+    <Layout title='Registar' hideNavbar>
+      <Container maxWidth='sm' className={classes.root}>
+        <Paper className={classes.paper}>
+          <div className={classes.backContainer}>
+            <MUILink href='#' onClick={router.back} className={classes.backLink} underline='none'>
+              <BackIcon />
+              <strong>Voltar</strong>
+            </MUILink>
+          </div>
+          <LogoSvg className={classes.logo} />
+          <Typography variant='h4' component='h1'>
+            Registar
+          </Typography>
+          {!!error && (
+            <Alert onClose={() => setError('')} severity='error' className={classes.alert}>
+              {error}
+            </Alert>
+          )}
+          <form noValidate>
+            <div className={classes.fieldDiv}>
               <TextField
                 id='username'
                 label='Nome de utilizador'
@@ -47,9 +158,11 @@ const SignUp = () => {
                 onChange={onUsernameChange}
                 type='text'
                 variant='outlined'
+                fullWidth
+                error={username.length > 0 && usernameError}
               />
             </div>
-            <div>
+            <div className={classes.fieldDiv}>
               <TextField
                 id='email'
                 label='Email'
@@ -57,9 +170,11 @@ const SignUp = () => {
                 onChange={onEmailChange}
                 type='email'
                 variant='outlined'
+                fullWidth
+                error={email.length > 0 && emailError}
               />
             </div>
-            <div>
+            <div className={classes.fieldDiv}>
               <TextField
                 id='password'
                 label='Palavra Passe'
@@ -67,12 +182,48 @@ const SignUp = () => {
                 onChange={onPasswordChange}
                 type='password'
                 variant='outlined'
+                fullWidth
+                error={password.length > 0 && passwordError}
+                helperText='Deverá conter, pelo menos, 8 caracteres e um dígito'
               />
             </div>
-            <Button color='primary' onClick={onSubmit}>
-              Criar conta
-            </Button>
+            <div className={classes.fieldDiv}>
+              <TextField
+                id='password'
+                label='Reintroduzir Palavra Passe'
+                value={passwordConfirmation}
+                onChange={onPasswordConfirmationChange}
+                type='password'
+                variant='outlined'
+                fullWidth
+                error={passwordConfirmation.length > 0 && passwordConfirmation !== password}
+              />
+            </div>
+            <div className={classes.buttonArea}>
+              <Button
+                type='submit'
+                color='primary'
+                onClick={onSubmit}
+                variant='contained'
+                fullWidth
+                classes={{
+                  root: classes.submitButton,
+                  disabled: classes.buttonDisabled,
+                }}
+                disabled={loading}
+              >
+                Criar Conta
+              </Button>
+              {loading && <CircularProgress size={24} className={classes.loading} />}
+            </div>
           </form>
+          <Typography variant='body2'>
+            <Link href='/signin' passHref>
+              <MUILink>
+                <strong>Já tem uma conta? Inicie sessão!</strong>
+              </MUILink>
+            </Link>
+          </Typography>
         </Paper>
       </Container>
     </Layout>
