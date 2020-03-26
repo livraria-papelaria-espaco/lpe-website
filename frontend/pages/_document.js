8e9cc4bc294f09f3403e9e0ccbc27507a402d237
Diogo Correia <me@diogotc.com>
2020-03-26 17:15:49 +0000

    Add Tawk.to

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -20,6 +20,23 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script
+            async
+            type='text/javascript'
+            dangerouslySetInnerHTML={{
+              __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/5e7ce12935bcbb0c9aaab60c/default';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+              })();
+              `,
+            }}
+          />
         </body>
       </html>
     );
