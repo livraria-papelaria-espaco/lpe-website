7ea3ea4b9f52a5665190290c310fa6f6fb1eb40d
Diogo Correia <me@diogotc.com>
2020-03-27 22:33:08 +0000

    Make ToS and Privacy Policy configurable (closes #5)

--- frontend/pages/legal/privacy.js
+++ frontend/pages/legal/privacy.js
@@ -1,8 +1,33 @@
+import { useQuery } from '@apollo/react-hooks';
+import { Typography } from '@material-ui/core';
+import gql from 'graphql-tag';
+import ErrorPage from 'next/error';
 import React from 'react';
 import Layout from '~/components/Layout';
+import Markdown from '~/components/text/Markdown';
 
-const TermsOfService = () => {
-  return <Layout title='Termos de Serviço'>//TODO</Layout>;
+const PrivacyPolicy = () => {
+  const { data, loading, error } = useQuery(TOS_QUERY);
+
+  if (error) return <ErrorPage statusCode={500} />;
+  if (loading) return null;
+
+  return (
+    <Layout title='Política de Privacidade'>
+      <Typography variant='h3' component='h1'>
+        Política de Privacidade
+      </Typography>
+      <Markdown children={data.privacyPolicy.privacyPolicy} />
+    </Layout>
+  );
 };
 
-export default TermsOfService;
+const TOS_QUERY = gql`
+  query TOS_QUERY {
+    privacyPolicy {
+      privacyPolicy
+    }
+  }
+`;
+
+export default PrivacyPolicy;
--- frontend/pages/legal/tos.js
+++ frontend/pages/legal/tos.js
@@ -1,8 +1,33 @@
+import { useQuery } from '@apollo/react-hooks';
+import { Typography } from '@material-ui/core';
+import gql from 'graphql-tag';
+import ErrorPage from 'next/error';
 import React from 'react';
 import Layout from '~/components/Layout';
+import Markdown from '~/components/text/Markdown';
 
 const TermsOfService = () => {
-  return <Layout title='Termos de Serviço'>//TODO</Layout>;
+  const { data, loading, error } = useQuery(TOS_QUERY);
+
+  if (error) return <ErrorPage statusCode={500} />;
+  if (loading) return null;
+
+  return (
+    <Layout title='Termos de Serviço'>
+      <Typography variant='h3' component='h1'>
+        Termos de Serviço
+      </Typography>
+      <Markdown children={data.termsOfService.termsOfService} />
+    </Layout>
+  );
 };
+
+const TOS_QUERY = gql`
+  query TOS_QUERY {
+    termsOfService {
+      termsOfService
+    }
+  }
+`;
 
 export default TermsOfService;
