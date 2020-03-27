4e9d8f8ea675a9cc7c14c5bad2e8e900abad0a42
Diogo Correia <me@diogotc.com>
2020-03-27 11:57:34 +0000

    Redesign product list layout

--- frontend/components/products/ProductList.js
+++ frontend/components/products/ProductList.js
@@ -1,13 +1,25 @@
 import { Grid } from '@material-ui/core';
 import React from 'react';
 import ProductCard from './ProductCard';
+import ProductSkeleton from './ProductSkeleton';
 
-const ProductList = ({ products }) => {
+const ProductList = ({ products, loading }) => {
+  if (loading)
+    return (
+      <Grid container direction='row' justify='flex-start' alignItems='center' spacing={3}>
+        {[...Array(3)].map((_, i) => (
+          <Grid item xs={6} md={4} lg={3} key={i}>
+            <ProductSkeleton />
+          </Grid>
+        ))}
+      </Grid>
+    );
+
   if (products.length === 0)
     return <p>Não foram encontrados produtos. Experimente remover alguns filtros.</p>;
 
   return (
-    <Grid container direction='row' justify='center' alignItems='center' spacing={3}>
+    <Grid container direction='row' justify='flex-start' alignItems='stretch' spacing={3}>
       {products.map((res) => (
         <Grid item xs={6} md={4} lg={3} key={res.slug}>
           <ProductCard product={res} />
