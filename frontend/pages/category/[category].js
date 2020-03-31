c425e8e78bf0ed172d993b5cf580b23af3aaf0d8
Diogo Correia <me@diogotc.com>
2020-03-31 16:35:14 +0100

    Enable SSR for the categories page (closes #14)

--- frontend/pages/category/[category].js
+++ frontend/pages/category/[category].js
@@ -1,11 +1,13 @@
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
 import React from 'react';
 import Layout from '~/components/Layout';
 import FilterToolbar from '~/components/products/filters/FilterToolbar';
 import ProductQuery from '~/components/products/ProductQuery';
 import { useProductFilters } from '~/hooks/useProductFilters';
+import { fetchAPI } from '~/lib/graphql';
 
 const GET_CATEGORY_FROM_SLUG = gql`
   query GET_CATEGORY_FROM_SLUG($category: String!) {
@@ -15,15 +17,21 @@ const GET_CATEGORY_FROM_SLUG = gql`
   }
 `;
 
-const CategoryPage = () => {
+const GET_CATEGORIES = gql`
+  query GET_CATEGORIES {
+    categories {
+      slug
+    }
+  }
+`;
+
+const CategoryPage = ({ categoryName }) => {
   const { delayedSearch, priceRange, sort } = useProductFilters();
   const router = useRouter();
   const { category } = router.query;
   const { data } = useQuery(GET_CATEGORY_FROM_SLUG, { variables: { category } });
 
-  // TODO?
-  const categoryTitle =
-    (data && data.categoryBySlug && data.categoryBySlug.name) || category || 'Produtos';
+  const categoryTitle = (data && data.categoryBySlug && data.categoryBySlug.name) || categoryName;
 
   return (
     <Layout title={categoryTitle} showStoreNav>
@@ -36,6 +44,29 @@ const CategoryPage = () => {
       />
     </Layout>
   );
+};
+
+CategoryPage.propTypes = {
+  categoryName: PropTypes.string,
+};
+
+CategoryPage.defaultProps = {
+  categoryName: undefined,
+};
+
+export const getStaticPaths = async () => {
+  const data = await fetchAPI(GET_CATEGORIES);
+  const paths = (data.categories || []).map((v) => ({ params: { category: v.slug } }));
+  return { paths, fallback: true };
+};
+
+export const getStaticProps = async (context) => {
+  const data = await fetchAPI(GET_CATEGORY_FROM_SLUG, {
+    variables: { category: context.params.category },
+  });
+  return {
+    props: { categoryName: (data && data.categoryBySlug && data.categoryBySlug.name) || null },
+  };
 };
 
 export default CategoryPage;
