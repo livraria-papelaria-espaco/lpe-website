6ca48430a744d5ccaa89054024c7be22eb989e7d
Diogo Correia <me@diogotc.com>
2020-05-07 16:28:37 +0100

    Add newsroom test
    Signed-off-by: Diogo Correia <me@diogotc.com>

---
+++ frontend/components/newsroom/__tests__/NewsArticle.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NewsArticle from '../NewsArticle';
+
+describe('NewsArticle (components/newsroom/NewsArticle', () => {
+  it('Renders correctly and matches snapshot', () => {
+    const title = 'Lorem Ipsum';
+    const content = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.  
+Donec non eros dignissim, dignissim erat eget, tincidunt magna.
+Ut nisl risus, *posuere* non **finibus quis, consequat vitae** lacus.`;
+    const date = new Date('2019-02-19T15:25:00.000Z');
+
+    const wrapper = shallow(<NewsArticle title={title} content={content} date={date} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
