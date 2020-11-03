bc2fae6b2174151ebc77f3db96ff9cd7c5caf183
Diogo Correia <me@diogotc.com>
2020-11-03 22:35:12 +0000

    Fix Fnac images not being crawled
    Fnac recently added an anti crawler system, but adding some headers
    prevents it from being triggered.
    
    Signed-off-by: Diogo Correia <me@diogotc.com>

--- backend/plugins/metadata-fetcher/services/metadata-fetcher.js
+++ backend/plugins/metadata-fetcher/services/metadata-fetcher.js
@@ -12,6 +12,11 @@ const WOOK_REGEX = /<script type="application\/ld\+json">[^]*?({[^]+})[^]*?<\/sc
 const NEWLINE_REGEX = /\n/g;
 const FNAC_SEARCH_REGEX = /<a href="(.+?)" class=".*?Article-title js-minifa-title js-Search-hashLink.*?">.+?<\/a>/;
 const FNAC_REGEX = /<script type="application\/json" class="js-configuration">[^]*?({.+})[^]*?<\/script>/;
+const FNAC_HEADERS = {
+  'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
+  'Accept-Language': 'en-US,en;q=0.9',
+};
 
 const fetchFnacImage = async (url, i) => {
   try {
@@ -27,13 +32,19 @@ const fetchFnacImage = async (url, i) => {
 const fetchImagesFromFnac = async (isbn) => {
   try {
     const searchResponse = await axios.get(
-      `https://www.fnac.pt/SearchResult/ResultList.aspx?Search=${isbn}`
+      `https://www.fnac.pt/SearchResult/ResultList.aspx?Search=${isbn}`,
+      {
+        headers: FNAC_HEADERS,
+      }
     );
     const productUrl = (FNAC_SEARCH_REGEX.exec(searchResponse.data) || [])[1];
 
-    const response = await axios.get(productUrl);
+    const response = await axios.get(productUrl, {
+      headers: FNAC_HEADERS,
+    });
     const dataString = (FNAC_REGEX.exec(response.data) || [])[1];
     const data = JSON.parse(dataString);
+    console.log(data);
     const images = await Promise.all(
       data.productData.images.map((imgSet, i) =>
         fetchFnacImage(imgSet.zoom || imgSet.image || imgSet.thumb)
@@ -41,6 +52,7 @@ const fetchImagesFromFnac = async (isbn) => {
     );
     return images.filter((i) => !!i);
   } catch (e) {
+    console.log(e);
     return [];
   }
 };
@@ -86,6 +98,7 @@ module.exports = {
 
   fetchAndUploadImages: async (isbn, slug) => {
     const images = await fetchImagesFromFnac(isbn);
+    console.log('images', images);
 
     const uploadService = strapi.plugins['upload'].services.upload;
     const { optimize } = strapi.plugins['upload'].services['image-manipulation'];
