686f00b2a281a9c98c783239d8ba3d69611fa00e
Diogo Correia <me@diogotc.com>
2022-04-20 17:43:22 +0100

    Update production configuration for strapi email plugin

--- backend/config/env/production/plugins.js
+++ backend/config/env/production/plugins.js
@@ -1,9 +1,7 @@
 module.exports = {
   email: {
-    provider: 'lpemailer',
-    providerOptions: {
-      logger: false,
-    },
+    provider: 'nodemailer',
+    providerOptions: {},
     settings: {
       defaultFrom: '"Livraria e Papelaria Espaço" <no-reply@lpespaco.pt>',
       defaultReplyTo: '"Livraria e Papelaria Espaço" <geral@lpespaco.pt>',
